dd0f49828eab92c36148e3ad690c114c032b635d
ramana24 <yellapuramana@gmail.com>
2021-07-27 08:57:54 +0530

    code clean up- v3

--- fioriuxdashboard/webapp/controller/View1.controller.js
+++ fioriuxdashboard/webapp/controller/View1.controller.js
@@ -1,16 +1,22 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/odata/v2/ODataModel",
-	"sap/ui/model/json/JSONModel"
+	"sap/ui/model/json/JSONModel",
+    'ux/fioriuxdashboard/js/Formatter'
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,ODataModel,JSONModel) {
+	function (Controller,ODataModel,JSONModel,Formatter) {
 		"use strict";
 
 		return Controller.extend("ux.fioriuxdashboard.controller.View1", {
+			Formatter:Formatter, // loading outside formatter into our file
 			onInit: function () {
+
+this.chartFunction();
+
+				// 0
 				var oView= this.getView();
 
 				// ECC Block tile
@@ -32,10 +38,16 @@ sap.ui.define([
 
 				//	oView.byId("crmcontainer").bindElement("/Products('AR-FB-1000')");
  
+
+				    // CRM block tile
+					var vServiceUrlPLM ="/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products('AR-FB-1009')";
+					var oPLMModel = new JSONModel(vServiceUrlPLM); // client side
+						oView.byId("PLMContainer").setModel(oPLMModel,"PLM"); // CRM Named model
 			},
 
 			onAfterRendering: function(){
 
+				//3
 				var oView= this.getView();
 				var vServiceUrl="/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/";
 				var oSRMModel= new sap.ui.model.odata.v2.ODataModel(vServiceUrl); // server side
@@ -45,6 +57,178 @@ sap.ui.define([
 				oView.byId("idSRMContainer").setModel(oSRMModel);
 
 				oView.byId("idSRMContainer").bindElement("/Products('AR-FB-1003')");
-			}
+			},
+
+			onBeforeRendering:function(){
+//2
+				     // SF block tile
+					 var oView= this.getView();
+					 var vServiceUrlSF ="/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products('AR-FB-1007')";
+					 var oSFModel = new JSONModel(vServiceUrlSF); // client side
+						 oView.byId("SFContainer").setModel(oSFModel,"SF"); // SF Named model
+
+
+						 
+				     // concur block tile
+					 var oView= this.getView();
+					 var vServiceUrlConcur ="/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products('AR-FB-1010')";
+					 var oCModel = new JSONModel(vServiceUrlConcur); // client side
+						 oView.byId("concurContainer").setModel(oCModel,"Concur"); // concur Named model
+
+
+
+						 		 
+				     // fieldglass block tile
+					 var oView= this.getView();
+					 var fieldglass ="/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products('AR-FB-1011')";
+					 var oFGModel = new JSONModel(fieldglass); // client side
+						 oView.byId("idFGContainer").setModel(oFGModel,"FG"); // FG Named model
+
+						 		 
+				     // BW block tile
+					 var oView= this.getView();
+					 var vServiceUrlBW ="/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products('AR-FB-1012')";
+					 var oBWModel = new JSONModel(vServiceUrlBW); // client side
+						 oView.byId("idBWContainer").setModel(oBWModel,"BW"); // BW Named model
+
+						 		 
+				     // ariba block tile
+					 var oView= this.getView();
+					 var vServiceUrlariba ="/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products('AR-FB-1013')";
+					 var oAModel = new JSONModel(vServiceUrlariba); // client side
+						 oView.byId("idAribaContainer").setModel(oAModel,"Ariba"); // Ariba Named model
+
+					
+			},
+
+			onOpenSystemErrors:function(oEvent){
+
+			
+					
+				// @ts-ignore
+				var oView = this.getView();
+				var oDialog = oView.byId("idSysError");
+			  //  oDialog.setModel(oModelFetch, "tableModel");
+				// create dialog lazily
+				if (!oDialog) {
+					// create dialog via fragment factory
+					// @ts-ignore
+					oDialog = sap.ui.xmlfragment(oView.getId(), "ux.fioriuxdashboard.view.SysError", this);
+					oView.addDependent(oDialog);
+				}
+			   
+				oDialog.open();
+
+		 	var oModel = new JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Suppliers?$format=json");
+		  this.getView().byId("idSysError").setModel(oModel,"SError");
+
+
+			},
+			onClose:function(){
+				var oView = this.getView();
+				var oDialog = oView.byId("idSysError");
+
+				oDialog.close();
+			},
+
+			onOpenAppDialog: function(oEvent){
+				// @ts-ignore
+				// @ts-ignore
+				var   IvSystem ;
+		
+					
+				   // @ts-ignore
+				   var oView = this.getView();
+				   var oDialog = oView.byId("ADialog");
+				 //  oDialog.setModel(oModelFetch, "tableModel");
+				   // create dialog lazily
+				   if (!oDialog) {
+					   // create dialog via fragment factory
+			           // @ts-ignore
+			           oDialog = sap.ui.xmlfragment(oView.getId(), "ux.fioriuxdashboard.view.AppDialog", this);
+			           oView.addDependent(oDialog);
+				   }
+				  
+                   oDialog.open();
+
+				   var oModel = new JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Suppliers?$format=json");
+				this.getView().byId("ADialog").setModel(oModel,"oModel");
+
+
+			
+
+               },
+
+			   chartFunction :function(){
+
+// column chart
+
+ /*Total -Summary*/
+ var oVizFrame = this.getView().byId("idVizColumn");
+ var oModel = new sap.ui.model.json.JSONModel();
+var data = {
+	   'Count' : [
+		   {"Status": "Total","Value": "1234"},
+		   {"Status": "Inbound","Value": "9127"},
+		  
+			{"Status": "Outbound","Value": "15957"},
+			{"Status": "Profit","Value": "20000"},
+			
+		  ]};
+oModel.setData(data);
+
+var oDataset = new sap.viz.ui5.data.FlattenedDataset({
+   dimensions : [{
+	   name : 'Status',
+	   value : "{Status}"}],
+				  
+   measures : [{
+	   name : 'Count',
+	   value : '{Value}'} ],
+				
+   data : {
+	   path : "/Count"
+   }
+});		
+oVizFrame.setDataset(oDataset);
+oVizFrame.setModel(oModel);	
+oVizFrame.setVizType('line');
+
+
+oVizFrame.setVizProperties({
+
+   plotArea: {
+  	colorPalette : d3.scale.category20().range,
+   dataLabel: {
+				   visible: "true"
+			   }
+			
+	   }});
+
+var feedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+	 'uid': "valueAxis",
+	 'type': "Measure",
+	 'values': ["Count"],
+	 'label':"true"
+   }), 
+   feedDataAxis =   new sap.viz.ui5.controls.common.feeds.FeedItem({
+	'uid': "DataAxis",
+	'type': "Measure",
+	'values': ["Count"],
+	'label':"true"
+  }), 
+   feedCategoryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+	 'uid': "categoryAxis",
+	 'type': "Dimension",
+	 'values': ["Status"]
+   });
+oVizFrame.addFeed(feedValueAxis);
+oVizFrame.addFeed(feedCategoryAxis);
+oVizFrame.addFeed(feedDataAxis);
+
+
+
+
+			   }
 		});
 	});
---
+++ fioriuxdashboard/webapp/js/Formatter.js
@@ -0,0 +1,130 @@
+sap.ui.define(function() {
+	"use strict";
+
+	var Formatter = {
+
+		weightState :  function (fMeasure, sUnit) {
+
+			// Boarder values for different status of weight
+			var fMaxWeightSuccess = 1;
+			var fMaxWeightWarning = 5;
+			var fAdjustedMeasure = parseFloat(fMeasure);
+
+			// if the value of fMeasure is not a number, no status will be set
+			if (isNaN(fAdjustedMeasure)) {
+				return "None";
+			} else {
+
+				if (sUnit === "G") {
+					fAdjustedMeasure = fMeasure / 1000;
+				}
+
+				if (fAdjustedMeasure < 0) {
+					return "None";
+				} else if (fAdjustedMeasure < fMaxWeightSuccess) {
+					return "Success";
+				} else if (fAdjustedMeasure < fMaxWeightWarning) {
+					return "Warning";
+				} else {
+					return "Error";
+				}
+			}
+        },
+        formatDate : function(inpt) {
+         		   if(inpt)
+         		   {
+         			   var value = inpt.toString();
+         			   
+         			   if (value.startsWith("/")) 
+         			   {
+         				   var sJsonDate = value;
+         				   
+         				   var sNumber = sJsonDate.replace(/[^0-9]+/g,'');
+         				   var iNumber = sNumber * 1; 
+         				   var oDate = new Date(iNumber);
+         				   
+         				   return oDate;
+         		       }
+         			   else
+         			   {
+         				   return inpt; 
+         			   }
+         		   }
+         		   else
+         		   {
+         			   return inpt;
+         		   }
+                },
+                formatJSONDate:function(Input){
+							if(Input){
+							var dateString = Input.substring(6,19);
+							var currentTime = new Date(parseInt(dateString ));
+							var month = currentTime.getMonth() + 1;
+							var day = currentTime.getDate();
+							var year = currentTime.getFullYear();
+							var hour = currentTime.getHours();
+							var minutes = currentTime.getMinutes();
+							var seconds = currentTime.getSeconds();
+							var date = day + "-" + month + "-" + year +"  "+ hour +":" + minutes +":"+ seconds;	
+						//	var date = "" + hour +":" + minutes +":"+ seconds;
+						//	var date = "" + hour +":" + minutes;
+
+                        var date2=  year + "/" + month + "/" + day;
+												return date2;
+							}
+							else
+								{
+								return Input;
+								}
+						},
+						
+						SystemStatus:function(oValue, oThreshold)
+						{
+							var Value=parseInt(oValue);
+							var Threshold = parseInt(oThreshold);
+							
+							if(Value < Threshold){
+								return "Success"	
+							}
+							else if(Value >= Threshold)
+								{
+								return "Error"
+								}
+							else
+								{
+								return "None"
+								}
+							
+						},
+						
+						QueueDepthStatus:function(oQDepth, oQThreshold,oQueueAge,oThreadAge)
+						{
+							var QueueAge = oQueueAge;
+							var ThreadAge = oThreadAge;
+							
+							if(QueueAge == "X" || ThreadAge == "X"){
+								return "Error"
+							}
+							else {
+							var QDepth = parseInt(oQDepth);
+							var QThreshold= parseInt(oQThreshold);
+							
+							if(QDepth < QThreshold){
+								return "Good"
+								
+							}else if(QDepth >= QThreshold)
+								{
+								return "Error"
+								}
+							else
+								{
+								return "Neutral"
+								}
+							}
+						},
+						
+	};
+
+	return Formatter;
+
+}, /* bExport= */ true);
